838215345fd6e8c9c158f5d2c46023fc28875e75
Pratyush <pratyushsingh.1602@gmail.com>
2021-05-01 16:50:11 +0530

    comments addded

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- let userScore = 0;
+let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
@@ -9,17 +9,21 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 
+// this will choose random value from Rock,Paper or Scissors
  function getComputerChoice() {
   const choices = ['r', 'p', 's'];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
 
+//this will convert 'r','p','s' to "Rock","Paper" and "Scissors"
  function convertToWord(letter) {
  	if (letter === "r") return "Rock";
  	if (letter === "p") return "Paper";
  	return "Scissors";
  }
+
+//When user will win, it will increase the win count and appear on screen
 
  function win(userChoice, computerChoice){
  	userScore++;
@@ -31,7 +35,9 @@ const scissors_div = document.getElementById("s");
 
  }
 
- function lose(userChoice, computerChoice){
+//When user will lose,this will increase the lost count and appear on screen
+
+function lose(userChoice, computerChoice){
  	computerScore++;
  	userScore_span.innerHTML = userScore;
  	computerScore_span.innerHTML = computerScore;
@@ -40,17 +46,21 @@ const scissors_div = document.getElementById("s");
  	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord} You Lost..💩"`;
  }
 
+//This function relates when there will be draw between user choice  and computer choice
+
  function draw(userChoice, computerChoice){
  	const smallUserWord = "user".fontsize(4).sub();
  	const smallCompWord = "comp".fontsize(4).sub();
  	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals to ${convertToWord(computerChoice)}${smallCompWord} It's Draw..!"`;
  }
 
+//All the possible combination are included here which will make changes in Win ,Lose or Draw function
+
 function game( userChoice){
 	const computerChoice = getComputerChoice();
 	switch(userChoice + computerChoice) {
 		case "rs":
-		case "pr":
+			case "pr":
 		case "sp":
 		 win(userChoice, computerChoice);
 		 break;
@@ -69,6 +79,8 @@ function game( userChoice){
 	}
 	
 }
+
+// main function is updated here
 
 function main(){
 	rock_div.addEventListener('click', function(){
